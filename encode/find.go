a3e7d9d819ccf42b2a95c6c1ee7a9d942a8111d7
wisdomfusion <wisdomfusion@gmail.com>
2021-05-14 11:18:19 +0800

    init; add encode.find

---
+++ encode/find.go
@@ -0,0 +1,19 @@
+package encode
+
+import (
+	"bufio"
+	"io"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+)
+
+func DeterminEncoding(r io.Reader) (e encoding.Encoding, name string, certain bool, err error) {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		return
+	}
+
+	e, name, certain = charset.DetermineEncoding(bytes, "")
+	return
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/WisdomFusion/go-encode
+
+go 1.16
+
+require (
+	golang.org/x/net v0.0.0-20210510120150-4163338589ed
+	golang.org/x/text v0.3.6
+)
